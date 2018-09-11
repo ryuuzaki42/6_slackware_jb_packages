e0c39f6534656d07f1f4d009d2bac5525bf89126
João Batista <joao42lbatista@gmail.com>
2018-09-11 18:52:49 -0300

    readme.r - Added, info about added dictionaries

---
+++ scripts_createPackage_JB/wps/readme.r
@@ -0,0 +1,6 @@
+## Adicionar dicionários:
+http://sidserra.blogspot.com/2016/06/adicionando-um-dicionario-pt-br-ao-wps.html
+http://wps-community.org/download/dicts
+
+unzip pt_BR.zip
+mv pt_BR/ /opt/kingsoft/wps-office/office6/dicts/
